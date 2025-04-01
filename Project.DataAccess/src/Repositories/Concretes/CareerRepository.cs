73eb704d5adfe6cb7a4fe1e4af04534d999003ec
MorveN11 <morales.patty.jose@gmail.com>
2025-04-01 10:41:58 -0400

    feat: add caching

--- Project.DataAccess/src/Repositories/Concretes/CareerRepository.cs
+++ Project.DataAccess/src/Repositories/Concretes/CareerRepository.cs
@@ -1,12 +1,13 @@
 using Project.DataAccess.Context;
 using Project.DataAccess.Entities.Concretes;
 using Project.DataAccess.Repositories.Interfaces;
+using Project.DataAccess.Services;
 
 namespace Project.DataAccess.Repositories.Concretes
 {
     public class CareerRepository : BaseEntityRelationRepository<Career, Student>, ICareerRepository
     {
-        public CareerRepository(PostgresContext context)
-            : base(context) { }
+        public CareerRepository(ICachingService cachingService, PostgresContext context)
+            : base(cachingService, context) { }
     }
 }
